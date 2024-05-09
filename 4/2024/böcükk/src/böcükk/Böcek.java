214d70e700c8e40b2626d9a6687d79c282c2d389
batugnrn <75200549+batugnrn@users.noreply.github.com>
2024-05-10 01:07:52 +0300

    Add files via upload

---
+++ 4/2024/böcükk/src/böcükk/Böcek.java
@@ -0,0 +1,11 @@
+package böcükk;
+
+public class Böcek extends Canli{
+
+    Böcek(int x,int y,int d){
+        super(x, y,d);
+        this.adi="C";
+        this.hayatta_mi=true;
+        
+    }
+}
---
+++ 4/2024/böcükk/src/böcükk/Main.java
@@ -0,0 +1,22 @@
+package böcükk;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+        Habitat h = new Habitat();
+        Oyun o = new Oyun();
+        o.oyna(h.HabitatOlustur(),h.satirdakiElemanSayisi());
+
+
+
+
+
+
+
+
+	}
+
+}
